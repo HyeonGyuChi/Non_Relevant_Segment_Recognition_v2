ff9de7c288ed8ad40fe5ad9aa100c2868e00dc63
bgpark1285 <bgpark@hutom.io>
2022-04-06 14:29:15 +0900

    db managing module update

---
+++ config/meta_db_config.py
@@ -0,0 +1,53 @@
+# Table elements
+# TEXT : string
+# INTEGER : int type
+# REAL : float or double
+# BOOL : 0 or 1
+
+table_elements = {
+    'SURGERY': 'TEXT',
+    'SURGERY_TYPE': 'TEXT',
+    'PATIENT': 'TEXT',
+    # 'VIDEO': 'TEXT',
+    'HOSPITAL': 'TEXT',
+    'SURGEON': 'TEXT',
+
+    'FPS': 'REAL',
+    'TOT_FRAME': 'INTERGER',
+    'RS_FRAME': 'INTERGER',
+    'NRS_FRAME': 'INTERGER',
+
+    'RS_RATIO': 'REAL',
+    'NRS_RATIO': 'REAL',
+    'NRS_CNT': 'INTEGER',
+
+    'TOT_DURATION': 'INTEGER',
+    'NRS_DURATION': 'INTEGER',
+
+    'DUP_TOT_RATIO': 'REAL',
+    'DUP_RS_RATIO': 'REAL',
+    'DUP_NRS_RATIO': 'REAL',
+
+    'ANNOTATION_V1': 'BOOL',
+    'ANNOTATION_V2': 'BOOL',
+    'ANNOTATION_V3': 'BOOL', 
+}
+
+
+# subset config
+# 특정 조건들... (nrs ratio 등 해당 조건에 맞는 데이터를 불러오도록 하기)
+subset_condition = {
+    'train': [
+        'SURGERY == "robot"',
+        # 'SURGERY_TYPE == "G_1"',
+        'SURGERY_TYPE IN ("G_1", "G_3")',
+        'FPS > 0',
+        'DUP_NRS_RATIO < 1.0',
+    ],
+    'val': [
+
+    ],
+    'test': [
+
+    ]
+}
--- script/db_test.py
+++ script/db_test.py
@@ -4,15 +4,31 @@
 def main():
     from config.base_opts import parse_opts
     from core.util.database import DBHelper
+    from config.meta_db_config import subset_condition
+    from core.dataset import SubDataset
 
     parser = parse_opts()
     args = parser.parse_args()
 
     db_helper = DBHelper(args)
+    db_helper.remove_table()
     db_helper.make_table()
+    db_helper.random_attr_generation()
+    df = db_helper.select(cond_info=None)
     
-    # 111
+    # print(df)
 
+    db_helper.update(
+        [['ANNOTATION_V3', 1],],
+        subset_condition['train'],
+        
+    )
+
+    dset = SubDataset(args, sample_type='boundary')
+
+    dset = SubDataset(args, sample_type='all')
+    
+    
 if __name__ == '__main__':
     if __package__ is None:
         import sys
